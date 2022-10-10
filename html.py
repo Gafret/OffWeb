4de6dca09767b166344786e70be07c6ecc82f606
Gafret <gafret_kelp@outlook.com>
2022-10-10 08:37:37 +0300

    Refactored few functions

--- html.py
+++ html.py
@@ -18,13 +18,15 @@ class HTMLObject:
     def __init__(self, start_url) -> None:
         self.url = start_url
         self.html = self.get_document()
-        self.hostname = urlparse(start_url).hostname
+        self.hostname = urlparse(start_url).hostname 
         self.local_path = ""
         self._depth = 0
 
 
     def _get_html(self):
+        
         response = requests.get(self.url)
+
         if response.status_code != 200:
             raise ConnectionError("Invalid response")
         
@@ -62,30 +64,48 @@ class HTMLObject:
     
     def get_interdom_links(self):
         # add support of relative links via regex 
-        # save ABSOLUTE links
         links = []
         for link in self.html.find_all("a", href=True):
-            if self.hostname in link["href"] or link["href"].startswith("/") or link["href"].startswith("./"): ## add support for links that go up the directory
+            if(self.hostname in link["href"] 
+            or link["href"].startswith("/") 
+            or link["href"].startswith("./")
+            or link["href"].startswith("../")): ## add support for links that go up the directory
                 links.append(link)
         
         return links
 
 
+    def get_abs_urls(self, links): # work around other protocols
+        abs_links = []
+
+        for link in links:
+            if link["href"].startswith("http"):
+                abs_links.append(link)
+            else:
+                abs_link = urljoin(self.url, link["href"])
+                abs_links.append(abs_link)
+        
+        return abs_links
+
+        
+    def change_hrefs(self, link_paths: dict):
+        with open(self.local_path, "r+", encoding="utf-8") as html_doc:
+            html = BeautifulSoup(html_doc.read(), "html.parser")
+
+            for link in html.find_all("a", href=True):
+                abs_link = urljoin(self.url, link["href"])
+                if abs_link in link_paths.keys():
+                    
+                    path = os.path.join("../../", link_paths[abs_link])  
+                    link["href"] = path
+            
+            html_doc.seek(0)
+            html_doc.write(str(html))
+            html_doc.truncate()
+
+
     def set_depth(self, depth):
         self._depth = depth
-
-
-    def change_hrefs(self, link):
-        pass
-        #link["href"] = f"..//..//level{self._depth - 1}//{link['href'].split('/')[-1]}"
-
-
-    def create_subdir(self, link): # move from class, doesnt correspond to object at all
-        dir = os.path.join(os.curdir, f"{link['href'].split('/')[-1]}")
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-        
-        return dir
 
     
     def download_html(self, *, level=None):
@@ -113,11 +133,7 @@ class HTMLObject:
 
         
 
-        
-
-        
-
-
+    
 
 
 class CSSObject:
@@ -151,7 +167,7 @@ class CSSObject:
 
         for i in range(len(self.css_links)):
             css_page = requests.get(self.css_links[i]["href"])
-            with open(f".//statics//{folder_name}//{title + str(i)}.css", "w") as css_file:
+            with open(f".//statics//{folder_name}//{title + str(i)}.css", "w", encoding="utf-8") as css_file:
                 css_file.write(css_page.text)
             
     
@@ -173,88 +189,55 @@ class CSSObject:
             self.css_links[i]["href"] = "../../statics/" + f"{folder_name}/" + title + str(i) + ".css"
 
 
-LOADED_PAGES = {}
-
-def get_abs_urls(start_url, links): # move to HTML class
-    abs_links = []
-
-    for link in links:
-        if link["href"].startswith("http"):
-            abs_links.append(link)
-        else:
-            abs_link = urljoin(start_url, link["href"])
-            abs_links.append(abs_link)
-    
-    return abs_links
-
-def create_subdir(depth):
-    dir = os.path.join(os.curdir, f"level{depth}")
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    
-    return dir
-
-
-def change_hrefs(html_path, link_paths: dict, url):
-    with open(html_path, "r+", encoding="utf-8") as html_doc:
-        html = BeautifulSoup(html_doc.read(), "html.parser")
-
-        for link in html.find_all("a", href=True):
-            abs_link = urljoin(url, link["href"])
-            if abs_link in link_paths.keys():
-                
-                path = os.path.join("../../", link_paths[abs_link])  
-                link["href"] = path
+LOADED_PAGES = {}            
         
-        html_doc.seek(0)
-        html_doc.write(str(html))
-        html_doc.truncate()
-            
-        
-    
-
 
 def download_subpages(html_obj: HTMLObject, depth: int) -> None:
 
     if depth == 0:
             return
 
-    interdom_links = get_abs_urls(html_obj.url, html_obj.get_interdom_links()) # delete duplicates
+    interdom_links = [link for link in html_obj.get_abs_urls(html_obj.get_interdom_links()) if type(link) == str]
 
 
     for link in interdom_links:
         if link not in LOADED_PAGES.keys():
 
             sub_page = HTMLObject(link)
-            sub_page.set_depth(depth)
             css_obj = CSSObject(sub_page)
+            
+            sub_page.set_depth(depth)
+
             css_obj.donwload_css()
             css_obj.change_css_paths()
+
             path = sub_page.download_html(level=depth)
             
             LOADED_PAGES.update({link:path})
             download_subpages(sub_page, depth-1)
 
-    change_hrefs(html_obj.local_path, LOADED_PAGES, html_obj.url)
+    html_obj.change_hrefs(LOADED_PAGES)
     
 
 
 
-url = "https://peps.python.org/pep-0008"
+url = "https://sourcemaking.com/refactoring/decompose-conditional"
 html_obj = HTMLObject(url)
 css_obj = CSSObject(html_obj)
 css_obj.donwload_css()
 
 css_obj.change_css_paths()
 
-path = html_obj.download_html()
+path = html_obj.download_html(level=0)
+
 
 LOADED_PAGES.update({url:path})
 
 
 
-download_subpages(html_obj, 2)
-print(LOADED_PAGES)
+download_subpages(html_obj, 1)
+html_obj.change_hrefs(LOADED_PAGES)
+
 
 
 
